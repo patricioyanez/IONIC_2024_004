fabcd71d89be606d16fc6b73a45e0b0d5a3964ba
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-08-20 13:47:15 -0400

    update

---
+++ MiApp004/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MiApp004',
+  webDir: 'www'
+};
+
+export default config;
