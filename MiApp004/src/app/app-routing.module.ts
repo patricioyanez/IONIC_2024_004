cdc6ba81adf7efb2963d22ae0dde174bf75e5a49
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-08-23 14:55:50 -0400

    update

--- MiApp004/src/app/app-routing.module.ts
+++ MiApp004/src/app/app-routing.module.ts
@@ -8,12 +8,24 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'menu',
     pathMatch: 'full'
   },
   {
     path: 'uno',
     loadChildren: () => import('./uno/uno.module').then( m => m.UnoPageModule)
+  },
+  {
+    path: 'menu',
+    loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule)
+  },
+  {
+    path: 'dos',
+    loadChildren: () => import('./dos/dos.module').then( m => m.DosPageModule)
+  },
+  {
+    path: 'tres',
+    loadChildren: () => import('./tres/tres.module').then( m => m.TresPageModule)
   },
 ];
 
