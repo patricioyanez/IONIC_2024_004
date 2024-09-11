e792cf7e3d7dd497c057bb73337c0de7c1094043
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-09-11 14:48:33 -0300

    update

--- MiApp004/src/app/ocho/ocho.page.ts
+++ MiApp004/src/app/ocho/ocho.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-ocho',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ocho.page.scss'],
 })
 export class OchoPage implements OnInit {
-
-  constructor() { }
+  n1 = '';
+  n2 = '';
+  n3 = '';
+  resultado : any ='';
+  constructor(private toastController:ToastController) { }
 
   ngOnInit() {
   }
+  // extension Error Lens
+  async promediar(){
+    this.resultado =  (this.n1 + this.n2 + this.n3);
+    this.resultado /=3; 
+    if(this.resultado < 40)
+    {
+      const toast = await this.toastController.create({
+        message : 'Ud reprobó con nota ' + this.resultado,
+        duration: 3000,
+        position: 'middle',
+        color   : 'danger'
+      });
+      await toast.present();
+    }
+    else
+    {
 
+    }
+  }
 }
