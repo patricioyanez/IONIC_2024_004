794eb018b2565a843afae32b7bc9a75550e1372b
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-10-02 14:32:16 -0300

    update

---
+++ miApp004_EA2/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'miApp004_EA2',
+  webDir: 'www'
+};
+
+export default config;
---
+++ miApp004_EA2/src/app/home/home.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  numero1:any;
+  constructor(private router:Router) {}
+  abrirPagina()
+  {
+    if(this.numero1 > 0)
+    {
+      this.router.navigate(['/segunda']);
+    }
+  }
+}
