0b7a9dac1ee19a7d3f6fe1873156d8d37f5b2ce3
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-10-09 14:38:22 -0300

    update

---
+++ miApp004_EA2/src/app/tercera-detalle/tercera-detalle.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tercera-detalle',
+  templateUrl: './tercera-detalle.page.html',
+  styleUrls: ['./tercera-detalle.page.scss'],
+})
+export class TerceraDetallePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
