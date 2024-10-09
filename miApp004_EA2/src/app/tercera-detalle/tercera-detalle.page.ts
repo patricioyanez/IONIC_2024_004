2a1ae9030ed13dc9d88c3e832c5f1354e4487551
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-10-09 14:49:15 -0300

    Update tercera-detalle.page.ts

--- miApp004_EA2/src/app/tercera-detalle/tercera-detalle.page.ts
+++ miApp004_EA2/src/app/tercera-detalle/tercera-detalle.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-tercera-detalle',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tercera-detalle.page.scss'],
 })
 export class TerceraDetallePage implements OnInit {
+  personas = [
+    {
+      id    : 1,
+      nombre: 'Juan',
+      edad  : 30,
+      foto  : 'https://img.freepik.com/foto-gratis/retrato-estudio-hombre-moreno-confianza_1187-5799.jpg?w=2000'
+    },
+    {
+      id    : 2,
+      nombre: 'Diana',
+      edad  : 28,
+      foto  : 'https://image.shutterstock.com/image-photo/happy-young-woman-sitting-on-260nw-2018571389.jpg'
 
-  constructor() { }
+    },
+    {
+      id    : 3,
+      nombre: 'Pedro',
+      edad  : 45,
+      foto  : 'https://img.freepik.com/foto-gratis/apuesto-hombre-empresario-sonriendo-alegre_176420-17877.jpg?w=2000'
+
+    }
+  ];
+  persona: any;
+  constructor(private activatedRoute:ActivatedRoute) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe( x=>{
+      const id = Number(x.get("id"));
+      this.persona = this.personas.find(xx => xx.id == id);
+    })
   }
 
 }
